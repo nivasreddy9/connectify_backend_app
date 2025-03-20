692aa47be19935272943395ef83b353e1a4f6668
nivasreddy9 <nivasreddy685@gmail.com>
2025-03-20 15:57:14 +0530

    cors updated

--- mainapp/app.js
+++ mainapp/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 // ✅ CORS Middleware (Placed at the top)
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend-connectifyy.vercel.app/",
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true  // ✅ Allows credentials (cookies, auth headers)
